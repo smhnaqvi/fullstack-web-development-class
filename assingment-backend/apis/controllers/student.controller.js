d68c258ff2d2b933d9bd0948fb8c9fd054e14f21
smhnaqvi <smhnaqvi111@gmail.com>
2026-05-23 15:38:52 +0500

    Implement CRUD operations for student management in the backend. Update routes to handle student creation, retrieval, updating, and deletion.

--- assingment-backend/apis/controllers/student.controller.js
+++ assingment-backend/apis/controllers/student.controller.js
@@ -1,11 +1,50 @@
-var studentModel = require("../models/student.model");
+const studentModel = require("../models/student.model");
 
-var createStudent = async (req, res) => {
+const createStudent = async (req, res) => {
     var student = new studentModel(req.body);
-    await student.save();
-    res.send(student);
+    await student.save().then(() => {
+        res.send(student);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+}
+
+const getAllStudents = async (req, res) => {
+    await studentModel.find().then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+}
+
+const getStudentById = async (req, res) => {
+    await studentModel.findById(req.params.id).then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+}
+
+const updateStudent = async (req, res) => {
+    await studentModel.findByIdAndUpdate(req.params.id, req.body).then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+}
+
+const deleteStudent = async (req, res) => {
+    await studentModel.findByIdAndDelete(req.params.id).then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
 }
 
 module.exports = {
-    createStudent
+    createStudent,
+    getAllStudents,
+    getStudentById,
+    updateStudent,
+    deleteStudent
 }
--- assingment-backend/routes/student.routes.js
+++ assingment-backend/routes/student.routes.js
@@ -2,6 +2,10 @@ var studentController = require("../apis/controllers/student.controller");
 var express = require("express");
 var router = express.Router();
 
-router.post("/create", studentController.createStudent);
+router.post("/student", studentController.createStudent);
+router.get("/students", studentController.getAllStudents);
+router.get("/student/:id", studentController.getStudentById);
+router.put("/student/:id", studentController.updateStudent);
+router.delete("/student/:id", studentController.deleteStudent);
 
 module.exports = router;
